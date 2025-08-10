15d712881ff329f348cdb1fb49e29ce2a2203486
Solomon Njobvu <solomonnjobvu1@gmail.com>
2025-08-10 23:17:23 +0200

    Delete route and not found page impl, bug fixes which make the web to be fully functional

--- app.py
+++ app.py
@@ -28,6 +28,25 @@ def index():
 
     return render_template('index.html', all_urls = all_url)
 
+@app.route("/<short_code>")
+def redirect_url(short_code):
+    url_data = get_url(short_code)
+    if url_data:
+        increment_visit_count(short_code)
+        return redirect(url_data[1])
+
+    return render_template('404.html'), 404
+
+
+@app.route('/delete/<short_code>', methods=["POST", "GET"])
+def delete(short_code):
+    delete_url(short_code)
+    return redirect('/')
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        debug=True,
+        host='0.0.0.0',
+        port=8080
+        )
