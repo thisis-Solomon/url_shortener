11d07c2ddc342724067030369e8dbf288461bc43
Solomon Njobvu <solomonnjobvu1@gmail.com>
2025-08-10 15:38:29 +0200

    handling the get, post and rendering templates

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
-from flask import Flask
+import random
+import string
+from flask import Flask, redirect, request, render_template
+from model import (
+    init_db,
+    get_url,
+    get_all_urls,
+    insert_url,
+    delete_url,
+    increment_visit_count
+    )
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Welcome to URL Shortener"
+init_db()
 
-@app.route('/about')
-def about():
-    return "This is the about page for the url shortener"
+def generate_short_code(length = 6):
+    return "".join(random.choice(string.ascii_letters + string.digits, k=length))
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == "POST":
+        original_url = request.form['url']
+        short_code = generate_short_code()
+        insert_url(original_url, short_code)
+        return redirect('/')
+    all_url = get_all_urls()
+
+    return render_template('index.html', all_url = all_url)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
